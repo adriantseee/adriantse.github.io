12777b9bf6f3ab273c9ff137f47ef28096c50f93
adraintseee <adriantseee2@gmail.com>
2024-01-26 00:17:01 +0800

    set up initial 3d model (cube)

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {Canvas} from '@react-three/fiber'
+import { RocketTimeline } from './RocketTimeline'
 
 function App() {
   return (
     <Canvas>
-      
+      <RocketTimeline />
     </Canvas>
   )
 }
