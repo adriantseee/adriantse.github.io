6d6e6d3bb03faa925ec90179385a32b3fdccda02
adraintseee <adriantseee2@gmail.com>
2024-01-26 18:55:45 +0800

    added rocket

--- src/RocketTimeline.jsx
+++ src/RocketTimeline.jsx
@@ -1,13 +1,25 @@
 import {OrbitControls} from "@react-three/drei"
+import {Rocket} from "./Rocket"
+import {useFrame} from "@react-three/fiber"
+import React from 'react'
 
 export const RocketTimeline = () => {
-    return(
+    const rocket = React.useRef()
+
+    function Rotate() {
+        useFrame(({ clock }) => {
+            rocket.current.rotation.y += 0.01; // Adjust the rotation speed as needed
+        })
+    }
+    Rotate();
+
+    return (
         <>
-        <OrbitControls />
-        <mesh>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
+            <ambientLight intensity={1} />
+            <mesh ref={rocket}>
+                <Rocket />
+            </mesh>
+            <OrbitControls />
         </>
     )
 }
