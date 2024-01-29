4f603b5b504778408c93d56c33e2a03bfd3a9b00
adraintseee <adriantseee2@gmail.com>
2024-01-29 09:44:56 +0800

    attempt to setup github pages

--- src/About.jsx
+++ src/About.jsx
@@ -1,27 +1,32 @@
-import {Container, Row, Col} from 'react-bootstrap';
+import {Row, Col} from 'react-bootstrap';
 import Spline from '@splinetool/react-spline';
+
 export const About = () => {
     return(
-        <div>
+        <Row>
             <div>
-                <div className = "name">
-                    <h1>ADRIAN</h1>
-                    <h1>TSE</h1>
+                <Col>
+                    <div className = "name">
+                        <h1>ADRIAN</h1>
+                        <h1>TSE</h1>
+                    </div>
+                    {/*Note that this is going to be a typing animation later on*/}
+                    <p>I am a student, 3D Designer, and Programmer</p>
+                </Col>
+                <div>
+                    <h1>A little about me...</h1>
+                    <div className = "Photo">
+                        {/*Put photo here, not working for now*/}
+                    </div>
+                    <div className = "info">
+                        <p>Hobbies 🏀: Basketball, Baseball, Golf, Building Keyboard</p>
+                        <p>Currently Learning 📖: React, SQL, AI/ML with Python</p>
+                    </div>
                 </div>
-                {/*Note that this is going to be a typing animation later on*/}
-                <p>I am a student, 3D Designer, and Programmer</p>
+                <Col>
+                    <Spline scene="https://prod.spline.design/gXDeJ3XTDyB2SQA8/scene.splinecode" />
+                </Col>
             </div>
-            <div>
-                <h1>A little about me...</h1>
-                <div className = "Photo">
-                    {/*Put photo here, not working for now*/}
-                </div>
-                <div className = "info">
-                    <p>Hobbies 🏀: Basketball, Baseball, Golf, Building Keyboard</p>
-                    <p>Currently Learning 📖: React, SQL, AI/ML with Python</p>
-                </div>
-            </div>
-            <Spline scene="https://prod.spline.design/gXDeJ3XTDyB2SQA8/scene.splinecode" />
-        </div>
+        </Row>
     )
 }
--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { Suspense } from 'react'
 import './App.css'
-import {Canvas} from '@react-three/fiber'
-import {Track} from './Track.jsx'
-import { OrbitControls } from '@react-three/drei'
-import { Car } from './Car.jsx'
 import { About } from './About.jsx'
 
 
