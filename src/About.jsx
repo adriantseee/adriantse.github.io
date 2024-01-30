5831fe61af1092832a26f5f04eeb097159a25a23
adraintseee <adriantseee2@gmail.com>
2024-01-30 09:06:42 +0800

    added typing animation

--- src/About.jsx
+++ src/About.jsx
@@ -1,32 +1,43 @@
 import {Row, Col} from 'react-bootstrap';
 import Spline from '@splinetool/react-spline';
+import {TypeAnimation} from 'react-type-animation'
 
 export const About = () => {
     return(
-        <Row>
-            <div>
-                <Col>
-                    <div className = "name">
-                        <h1>ADRIAN</h1>
-                        <h1>TSE</h1>
+        <div className = "hero">
+            <div className = "about">
+                <div className = "title">
+                    <div>
+                        <div className = "name">
+                            <h1>ADRIAN</h1>
+                            <h1>TSE</h1>
+                        </div>
+                        {/*Note that this is going to be a typing animation later on*/}
+                        {/*<p id="description">I am a student, 3D Designer, and Programmer</p>*/}
+                        <TypeAnimation 
+                        sequence={
+                            ['Student', 3000, '3D Designer', 3000, 'Programmer', 3000]
+                        }
+                        speed={50}
+                        style={{ fontSize: '24px'}}
+                        repeat={Infinity}
+                        />
                     </div>
-                    {/*Note that this is going to be a typing animation later on*/}
-                    <p>I am a student, 3D Designer, and Programmer</p>
-                </Col>
-                <div>
-                    <h1>A little about me...</h1>
-                    <div className = "Photo">
-                        {/*Put photo here, not working for now*/}
-                    </div>
-                    <div className = "info">
-                        <p>Hobbies 🏀: Basketball, Baseball, Golf, Building Keyboard</p>
-                        <p>Currently Learning 📖: React, SQL, AI/ML with Python</p>
+                    <div className = "model">
+                        <Spline scene="https://prod.spline.design/gXDeJ3XTDyB2SQA8/scene.splinecode" />
                     </div>
                 </div>
-                <Col>
-                    <Spline scene="https://prod.spline.design/gXDeJ3XTDyB2SQA8/scene.splinecode" />
-                </Col>
+                <div>
+                    <h2 id = "infotitle">A little about me...</h2>
+                    <div className = "additionalinfo">
+                        <img id = "pfp" src = "src/assets/pfp.png"/>
+                        <div className = "activities">
+                            <p>Hobbies 🏀: Basketball, Baseball, Golf, Building Keyboards</p>
+                            <p>Currently Learning 📖: React, SQL, AI/ML with Python</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </Row>
+        </div>
     )
 }
