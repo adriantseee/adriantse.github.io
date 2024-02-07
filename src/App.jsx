00ff7dcbf2fa699ede3f5d89a3995403dbcf37ac
adraintseee <adriantseee2@gmail.com>
2024-02-08 01:34:03 +0800

    cleaned up code + changed profile picture

--- src/App.jsx
+++ src/App.jsx
@@ -8,31 +8,6 @@ import {Projects} from './Projects.jsx'
 import {motion} from 'framer-motion'
 
 export const App = () => {
-  const [mousePos, setMousePos] = useState({
-    x: 0,
-    y: 0
-  });
-
-  useEffect(() => {
-    const mouseMove = e =>{
-      setMousePos({
-        x: e.clientX,
-        y: e.clientY
-      })
-    }
-    window.addEventListener('mousemove', mouseMove);
-
-    return() => {
-      window.removeEventListener('mousemove', mouseMove);
-  }
-  }, []);
-  
-  const variants = {
-    default: {
-      x: mousePos.x-16,
-      y: mousePos.y-16
-    }
-  }
   return (
     <>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
--- src/Contact.jsx
+++ src/Contact.jsx
@@ -3,7 +3,6 @@ export const Contact = () => {
         <div className = "contact-container">
         <div className = "call-to-action" style={{marginLeft: "10vw"}}>
             <h1 style={{letterSpacing: "8px", fontSize: "80px"}}>Like what you see?</h1>
-            <img id = "pfp" src = "/assets/pfp2.png"></img>
             <h1 style={{position: "absolute", letterSpacing: "8px", fontSize: "80px", marginLeft: "15vw", marginTop: "-3vh"}}><span>Let's get in </span><span style={{color: "#a44444"}}>touch</span></h1>
         </div>
         <div id="container-contact"style={{marginTop:"20vh"}}>
